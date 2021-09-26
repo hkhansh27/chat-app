beb869d9e0521ee318ec57f943f2831fb41b4ff7
hkhansh27 <khanh201011@gmail.com>
2021-09-26 13:26:57 +0700

    first commit

---
+++ lib/Screens/homescreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chat App"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: const [
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "Chats"),
+            Tab(text: "Status"),
+            Tab(text: "Calls"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: const [
+          Text("Camera"),
+          Text("Chats"),
+          Text("Status"),
+          Text("Calls"),
+        ],
+      ),
+    );
+  }
+}
